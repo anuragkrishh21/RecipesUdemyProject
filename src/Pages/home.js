dcfa4bc86ad26fe819261a1638f55b64adb577dd
Anuragkrishh <anuragkrishna95@gmail.com>
2019-06-09 15:16:57 +0530

    navbar & header

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
-
+import {Link} from 'react-router-dom'
+import Header from '../components/header';
 export default class home extends Component {
     render() {
         return (
-            <h4>
-                Hello from home page
-            </h4>
+            <Header title="Amazing Recipes">
+                <Link to="/recipes" 
+                className="text-uppercase btn btn-info
+                bt-lg mt-3">
+                    search recipes
+                </Link>
+            </Header>
         )
     }
 }
